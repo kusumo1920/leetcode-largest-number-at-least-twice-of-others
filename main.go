8b3e5f4fc2f5f7974f218b43d7276002027107e9
Kesit Budi Kusumo <kusumo1920@kesit-almalinux-pc.kernel.id>
2024-06-15 10:32:34 +0700

    feat: add iterative solution

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+func main() {
+	input := []int{0, 0, 3, 2}
+	output := dominantIndex(input)
+	fmt.Println(output)
+}
+
+func dominantIndex(nums []int) int {
+	doubleLargest := math.MinInt
+	secondDoubleLargest := math.MinInt
+	singleLargest := math.MinInt
+	singleLargestIndex := -1
+
+	for i, v := range nums {
+		if v >= singleLargest {
+			singleLargest = v
+			singleLargestIndex = i
+		}
+		if v*2 > doubleLargest {
+			secondDoubleLargest = doubleLargest
+			doubleLargest = v * 2
+		} else if v*2 > secondDoubleLargest {
+			secondDoubleLargest = v * 2
+		}
+	}
+
+	if singleLargest < secondDoubleLargest {
+		return -1
+	}
+
+	return singleLargestIndex
 }
